af27a979fce1ae31354b8e7af957ed60ca0644fe
danielle kefford <danielle_kefford@hotmail.com>
2014-12-04 18:40:15 -0500

    Added sound effect for new player bullet.

--- src/dodonpascii/core.clj
+++ src/dodonpascii/core.clj
@@ -1,24 +1,30 @@
 (ns dodonpascii.core
+  (:import [ddf.minim Minim])
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
-(defn make-game [w h]
+(defn make-game [w h m]
   {:w               w
    :h               h
    :status          :in-progress
-   :font           (q/create-font "Courier" 24)
    :player         {:x            (* w 0.5)
                     :y            (* h 0.8)
                     :direction-x  0
                     :direction-y  0}
    :player-bullets []
-   :events         []})
+   :events         []
+   :font           (q/create-font "Courier" 24)
+   :sounds         {:new-player-bullet (.loadFile m "resources/new-player-bullet.wav")}})
 
 (defn setup []
   (let [w (q/width)
-        h (q/height)]
+        h (q/height)
+        m (Minim.)]
     (q/smooth)
-    (make-game w h)))
+    (make-game w h m)))
+
+(defn clear-previous-events [state]
+  (assoc-in state [:events] []))
 
 (defn move-player [{{direction-x :direction-x
                      direction-y :direction-y} :player :as state}]
@@ -37,15 +43,19 @@
 
 (defn update-game [state]
   (-> state
+    (clear-previous-events)
     (move-player)
     (move-player-bullets)))
 
-(defn key-pressed [{{x :x y :y} :player :as state}
+(defn key-pressed [{{x :x y :y} :player
+                     sounds     :sounds :as state}
                     {key      :key
                      key-code :key-code :as event}]
   (case key
     :z
-      (update-in state [:player-bullets] conj {:x x :y y})
+      (do
+        (doto (:new-player-bullet sounds) .rewind .play)
+        (update-in state [:player-bullets] conj {:x x :y y}))
     :left
       (assoc-in state [:player :direction-x] -1)
     :right
